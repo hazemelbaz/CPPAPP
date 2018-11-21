136abb6323a1bfb3fc3864d8a7ddaaf8e8850b37
hazemelbaz <hazem_baz@hotmail.com>
2018-11-21 12:40:57 +0200

    This is My 2nd Cpp app
    this app will make summation of two numbers

---
+++ sum.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int main ()
+{
+	// This app will make sum for two Numbers
+	
+	int x=0; //this is initilization 
+	int y=0;
+	int z; // this is decleration
+
+	// here we make input from screen
+	cout<<"Enter 1st No.: ";
+	cin>> x;
+
+	cout<<"Enter 2nd No.: ";
+	cin>>y;
+
+	
+	z=x+y;
+	
+	cout<<"Sum is: "<<z;
+
+	return 0;
+	
+}
